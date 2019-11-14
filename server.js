984c799a45bb75b981f195ae5eb7bee691bd1e11
Brandon Nichols <brandonaronnichols@gamil.com>
2019-11-13 19:55:18 -0600

    added put route for accounts database, fixed problems with validateId

--- server.js
+++ server.js
@@ -24,6 +24,17 @@ server.post("/", validateAccount, async (req, res) => {
   }
 });
 
+server.put("/:id", validateId, validateAccount, async (req, res) => {
+  try {
+    const updateSuccess = await db("accounts")
+      .where("id", req.params.id)
+      .update(req.body);
+    res.status(200).json(updateSuccess);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 function validateAccount(req, res, next) {
   if (Object.keys(req.body).length) {
     if (!req.body.name || !req.body.budget) {
@@ -33,6 +44,22 @@ function validateAccount(req, res, next) {
     }
   } else {
     res.status(500).json({ message: "Error missing request body" });
+  }
+}
+
+async function validateId(req, res, next) {
+  const id = req.params.id;
+
+  try {
+    const account = await db("accounts").where({ id: id });
+    if (account.length > 0) {
+      req.account = account;
+      next();
+    } else {
+      res.status(404).json({ message: "Id of account does not exist" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
   }
 }
 
