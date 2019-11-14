a170b65e09a1715fdc9e1c94153b1d53c3d8055a
Brandon Nichols <brandonaronnichols@gamil.com>
2019-11-13 18:31:42 -0600

    added get route and post route endpoints for accounts database

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.get("/", async (req, res) => {
+  try {
+    const accounts = await db("accounts");
+    res.status(200).json(accounts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+server.post("/", validateAccount, async (req, res) => {
+  try {
+    const accounts = await db("accounts").insert(req.body);
+    res.status(200).json(accounts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+function validateAccount(req, res, next) {
+  if (Object.keys(req.body).length) {
+    if (!req.body.name || !req.body.budget) {
+      res.status(400).json({ message: "Data missing" });
+    } else {
+      next();
+    }
+  } else {
+    res.status(500).json({ message: "Error missing request body" });
+  }
+}
 
 module.exports = server;
